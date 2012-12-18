63911cf66f69ccd86e3a72f4f581a132022a5309
Martin Mägar <maxar.hol@gmail.com>
2012-12-18 15:38:17 +0200

    Added new stack functions

--- src/main/java/datastructures/Stack.java
+++ src/main/java/datastructures/Stack.java
@@ -2,20 +2,53 @@ package datastructures;
 
 public class Stack {
     private StackNode firstNode = null;
+    private int size = 0;
 
     public void push(int x) {
         StackNode newNode = new StackNode(x);
         newNode.setNextNode(firstNode);
         firstNode = newNode;
+        size++;
     }
 
     public int pop() {
         int val = firstNode.getValue();
         firstNode = firstNode.getNextNode();
+        size--;
         return val;
     }
 
     public boolean isEmpty() {
         return firstNode == null;
+    }
+
+    public boolean contains(int number) {
+        StackNode node = firstNode;
+        while (node != null) {
+            if (node.getValue() == number)
+                return true;
+            node = node.getNextNode();
+        }
+        return false;
+    }
+
+    public int peekAt(int index) {
+        StackNode currentNode = firstNode;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.getNextNode();
+        }
+        return currentNode.getValue();
+    }
+
+
+
+    public int[] toArray() {
+        int[] result = new int[size];
+        StackNode node = firstNode;
+        for (int i = 0; i < size; i++) {
+            result[i] = node.getValue();
+            node = node.getNextNode();
+        }
+        return result;
     }
 }
