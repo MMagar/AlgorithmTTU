c4c3bddff0c8bd53b6d8ea767359ce9731cace64
Martin M2gar <maxar.hol@gmail.com>
2012-12-20 12:26:29 +0200

    Added main method

--- src/main/java/knapsack/Main.java
+++ src/main/java/knapsack/Main.java
@@ -1,4 +1,14 @@
 package knapsack;
 
+import util.ItemFileUtil;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ItemFileUtil util = new ItemFileUtil("src/main/java/knapsack");
+        util.setFileIn("15.in");
+        util.setFileOut("15.out");
+        Assignments assignments = new Assignments(util);
+        assignments.solve();
+    }
 }
